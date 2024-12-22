328603b3ec16aa587b058efeefb5af6d04d27995
sofdf <sofiaaleshina@mail.ru>
2024-12-22 21:59:39 +0300

    b

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx,css}", // Убедитесь, что указаны все пути к вашим компонентам
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'lab4',
+  base:'/lab4/',
+  build: {
+    outDir: 'dist', // Папка, куда будет собираться проект
+  },
 
 })
