169f60d52e99fb6bd5785b76648676a2e2c22cc6
sofdf <sofiaaleshina@mail.ru>
2024-12-23 14:12:52 +0300

    b

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base:'/lab4/',
+  plugins: [react()],
   build: {
     outDir: 'dist', // Папка, куда будет собираться проект
   },
